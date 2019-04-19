0e06dc6e66624c8724ebec13ed1ff7cc333fec7d
Louxi3152 <34438507+Louxi3152@users.noreply.github.com>
2019-04-19 23:33:31 +0200

    advanced on lecture

--- src/main/main.go
+++ src/main/main.go
@@ -8,7 +8,13 @@ import (
 	"time"
 )
 
-const Pi = 3.14
+const (Pi = 3.14
+	// Create a huge number by shifting a 1 bit left 100 places.
+	// In other words, the binary number that is 1 followed by 100 zeroes.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
+)
 
 var (
 	c, python, java bool
@@ -29,6 +35,11 @@ func split(sum int) (x, y int) {
 	x = sum * 4 / 9
 	y = sum -x
 	return
+}
+
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
 }
 
 func main() {
@@ -65,11 +76,15 @@ func main() {
 	f := math.Sqrt(float64(a*a + b*b))
 	z := uint(f)
 	fmt.Println(a, b, z)
-	
+
 	const World = "世界"
 	fmt.Println("Hello", World)
 	fmt.Println("Happy", Pi, "Day")
 
 	const Truth = true
 	fmt.Println("Go rules?", Truth)
+
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
 }

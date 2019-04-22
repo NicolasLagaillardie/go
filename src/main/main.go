81f4933f610bef52992ff4f81799f532d2eb35ee
Louxi3152 <34438507+Louxi3152@users.noreply.github.com>
2019-04-22 21:37:11 +0200

    pointers and struct

--- src/main/main.go
+++ src/main/main.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"math/cmplx"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
@@ -22,7 +23,16 @@ var (
 	ToBe                   = false
 	MaxInt          uint64 = 1<<64 - 1
 	z                      = cmplx.Sqrt(-5 + 12i)
+	v1                     = Vertex{1, 2}  // has type Vertex
+	v2                     = Vertex{X: 1}  // Y:0 is implicit
+	v3                     = Vertex{}      // X:0 and Y:0
+	r                      = &Vertex{1, 2} // has type *Vertex
 )
+
+type Vertex struct {
+	X int
+	Y int
+}
 
 func add(x, y int) int {
 	return x + y
@@ -39,6 +49,7 @@ func split(sum int) (x, y int) {
 }
 
 func needInt(x int) int { return x*10 + 1 }
+
 func needFloat(x float64) float64 {
 	return x * 0.1
 }
@@ -48,6 +59,30 @@ func sqrt(x float64) string {
 		return sqrt(-x) + "i"
 	}
 	return fmt.Sprint(math.Sqrt(x))
+}
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	previous := 2.0
+	for i := 0; i < 50; i++ {
+		previous = z
+		z -= (z*z - x) / (2 * z)
+		if (previous-z) < 0.0000001 && (z-previous) < 0.0000001 {
+			fmt.Println("Number of iterations needed:", i)
+			return z
+		}
+	}
+	return z
 }
 
 func main() {
@@ -114,4 +149,78 @@ func main() {
 		sum += sum
 	}
 	fmt.Println(sum)
+
+	fmt.Println(sqrt(2), sqrt(-4))
+
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+
+	fmt.Println(Sqrt(2))
+
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		fmt.Printf("%s.\n", os)
+	}
+
+	fmt.Println("When's Saturday ?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+
+	/*	defer fmt.Println("world")
+		fmt.Println("hello")
+
+		fmt.Println("counting")
+		for i := 0; i < 10; i++ {
+			defer fmt.Println(i)
+		}
+		fmt.Println("done")*/
+
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
+
+	fmt.Println(Vertex{1, 2})
+
+	v := Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X)
+
+	q := &v
+	q.X = 1e9
+	fmt.Println(v)
+
+	fmt.Println(v1, r, v2, v3)
 }
